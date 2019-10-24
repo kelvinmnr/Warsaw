f061f1c1e41b96e52c1ae5a754e45c7f9556c271
nenekelvin <karvinmitchar@gmail.com>
2019-10-24 19:26:29 +0300

    generated modelwith fields

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @websites = [
+      ["http://railsgirls.com", "Kelvin"],
+      ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+      ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+    ]
   end
 end
