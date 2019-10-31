469631df5629b67055f1ae39996a676eff0df99a
nenekelvin <karvinmitchar@gmail.com>
2019-10-31 08:58:23 +0300

    persisting entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,4 +5,9 @@ class EntriesController < ApplicationController
 	 def show
 	 	@entry =Entry.find(params["id"])
 	 end
+	 def create
+  entry_params = params["entry"]
+  entry = Entry.create(entry_params)
+  redirect_to(entry_path(entry))
+end
 end
