7a29f942aa6e0181cb18478af95c3a20509f09ed
nenekelvin <karvinmitchar@gmail.com>
2019-11-05 08:34:53 +0300

    update method

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,6 +8,12 @@ class EntriesController < ApplicationController
 	 def edit
   			@entry = Entry.find(params["id"])
 	end
+	def update
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.find(params["id"])
+  entry.update(entry_params)
+  redirect_to(entry_path(entry))
+end
 	 def create
   entry_params = params["entry"]
   entry = Entry.create(entry_params)
